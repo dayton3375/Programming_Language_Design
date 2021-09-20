cdf74f948a9b4366b67dfed886ef24d6b54dcbdf
Dayton Dekam <daytondekam@wless-user-172019232049.temp.wsu.edu>
2021-09-20 15:43:24 -0700

    Added comments and deleted two redundant helper functions

--- Assignment1/HW1.hs
+++ Assignment1/HW1.hs
@@ -8,23 +8,21 @@ module HW1
 -- Q1 everyOther
 everyOther :: [a] -> [a]
 everyOther [] = []
-everyOther (x:xs) = helperEveryOther 1 (x:xs)
+everyOther (x:xs) = helperEveryOther 1 (x:xs) -- pass in 1 as a flag
                     where
                          helperEveryOther n [] = []
-                         helperEveryOther 0 (x:xs) = helperEveryOther 1 xs
-                         helperEveryOther 1 (x:xs) = x : helperEveryOther 0 xs
+                         helperEveryOther 0 (x:xs) = helperEveryOther 1 xs      -- given the 0 flag, skip to next
+                         helperEveryOther 1 (x:xs) = x : helperEveryOther 0 xs  -- given the 1 flag, copy character into list
 
 -- Q2(a) eliminateDuplicates
 eliminateDuplicates :: Eq a => [a] -> [a]
 eliminateDuplicates [] = []
-eliminateDuplicates (x:xs) = helperEliminateDuplicates (x:xs)
-                             where
-                                  helperEliminateDuplicates [] = []
-                                  helperEliminateDuplicates (x:xs) = x : (helperEliminateDuplicates (cleanList x (x:xs)))
-                                                                     where
-                                                                           cleanList c [] = []
-                                                                           cleanList c (x:xs) | (c == x) = cleanList c xs
-                                                                                              | otherwise = x : (cleanList c xs)
+eliminateDuplicates (x:xs) = x : (eliminateDuplicates (cleanList x (x:xs)))
+                              where -- cleanList takes a list and a token "c" and returns the list without any elements matching "c"
+                                   cleanList c [] = []
+                                   cleanList c (x:xs) | (c == x) = cleanList c xs
+                                                       | otherwise = x : (cleanList c xs)
+
 -- Q2(b) matchingSeconds
 matchingSeconds :: Eq t => t -> [(t, a)] -> [a]
 matchingSeconds v [] = []
@@ -34,24 +32,22 @@ matchingSeconds v ((a, b):xs) | (a == v) = b : matchingSeconds v xs
 -- Q2(c) clusterCommon
 clusterCommon :: (Eq t, Eq a) => [(t, a)] -> [(t, [a])]
 clusterCommon [] = []
-clusterCommon ((a, b):xs) = helperClusterCommon ((a, b):xs)
-                            where
-                                 helperClusterCommon [] = []
-                                 helperClusterCommon ((a, b):xs) = (a, eliminateDuplicates (matchingSeconds a ((a, b):xs))) : helperClusterCommon (eliminate a xs)
-                                                                   where
-                                                                        eliminate tok [] = []
-                                                                        eliminate tok ((a, b):xs) | (a == tok) = eliminate tok xs
-                                                                                                  | otherwise = (a, b) : eliminate tok xs
+clusterCommon ((a, b):xs) = (a, eliminateDuplicates (matchingSeconds a ((a, b):xs))) : clusterCommon (eliminate a xs)
+                              where     -- eliminate takes a list of tuples and a token "tok" and returns a list without any elements matching "tok"
+                                   eliminate tok [] = []
+                                   eliminate tok ((a, b):xs) | (a == tok) = eliminate tok xs
+                                                            | otherwise = (a, b) : eliminate tok xs
 
 -- Q3 maxNumCases
 maxNumCases :: (Num p, Ord p, Eq t) => [(a, [(t, p)])] -> t -> p
-maxNumCases [] month = 0
-maxNumCases ((a, b):xs) month = helperMaxCases 0 ((a, b):xs) month
+maxNumCases [] month = 0 -- if the month doesn't exist, 0 cases are reported
+maxNumCases ((a, b):xs) month = helperMaxCases 0 ((a, b):xs) month    -- cases are initialized to 0
                          where
-                              helperMaxCases cases [] month = cases
+                              helperMaxCases cases [] month = cases   -- at end of list, return cases found (0 if none found)
+                                                                      -- cases are changed to largest found
                               helperMaxCases cases ((a, b):xs) month | ((checkCounty b month) > cases) = helperMaxCases (checkCounty b month) xs month
                                                                      | otherwise = helperMaxCases cases xs month
-                                                                     where
+                                                                     where      -- checkCounty finds the month within the county's tuple and returns the cases
                                                                           checkCounty [] m = 0
                                                                           checkCounty ((a, b):xs) m | (a /= m) = checkCounty xs m
                                                                                                | otherwise = b
@@ -59,9 +55,9 @@ maxNumCases ((a, b):xs) month = helperMaxCases 0 ((a, b):xs) month
 -- Q4 groupIntoLists
 groupIntoLists :: [a] -> [[a]]
 groupIntoLists [] = []
-groupIntoLists (x:xs) = helperGroupIntoLists 1 (x:xs) []
-                         where
-                              helperGroupIntoLists n [] buf = [reverse buf]
+groupIntoLists (x:xs) = helperGroupIntoLists 1 (x:xs) []   -- 1 is the starting value of the "n" parameter
+                         where                             -- n represents the size of the list to return, and is decremented until 0 at each element
+                              helperGroupIntoLists n [] buf = [reverse buf]  -- at the end of list, return the buffer (remaining elements) regardless of n
                               helperGroupIntoLists n (x:xs) buf | (n == 0) = ((reverse buf) : helperGroupIntoLists ((size buf) + 1) (x:xs) []) 
                                                                 | otherwise = helperGroupIntoLists (n - 1) xs (x:buf)
                                                                  where
@@ -71,14 +67,13 @@ groupIntoLists (x:xs) = helperGroupIntoLists 1 (x:xs) []
 -- Q5 getSlice 
 getSlice :: Eq a => (a, a) -> [a] -> [a]
 getSlice (a, b) [] = []
-getSlice (a, b) (x:xs) = cutHead (a, b) (x:xs)
+getSlice (a, b) (x:xs) = cutHead (a, b) (x:xs) -- first must get the rest of the list after the delimiter "a"
                          where
                               cutHead (a, b) [] = []
-                              cutHead (a, b) (x:xs) | (x == a) = cutTail b xs []
+                              cutHead (a, b) (x:xs) | (x == a) = cutTail b xs []     -- then stop at the second delimiter "b"
                                                       | otherwise = cutHead (a, b) xs
                                                       where
                                                            cutTail b [] [] = []
                                                            cutTail b [] buf = reverse buf
                                                            cutTail b (x:xs) buf | (x == b) = reverse buf
                                                                                 | otherwise = cutTail b xs (x:buf)
-
