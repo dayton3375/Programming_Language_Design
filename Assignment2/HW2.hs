d159d415ac93f20d5e5f2da74fa2b76832317d08
Dayton Dekam <daytondekam@wless-user-172019002233.temp.wsu.edu>
2021-10-05 11:31:50 -0700

    HW2: P1 done and working on P2

--- Assignment2/HW2.hs
+++ Assignment2/HW2.hs
@@ -1,15 +1,28 @@
+-- Name: Dayton Dekam  (the only contributer)
+
 module HW2
      where
 
 {- 1. groupbyNTail - 10%-}
-
-
+groupbyNTail :: [a] -> Int -> [[a]]
+groupbyNTail [] n = []
+groupbyNTail iL n = grouptailhelper iL n [] []
+     where
+          grouptailhelper :: [a] -> Int -> [a] -> [[a]] -> [[a]]
+          grouptailhelper [] n bufsub buflists = reverse ((reverse bufsub) : buflists)
+          grouptailhelper (x:xs) n bufsub buflists | (length bufsub) >=n = grouptailhelper xs n (x:[]) ((reverse bufsub):buflists)
+                                                   | otherwise = grouptailhelper xs n (x:bufsub) buflists
 -----------------------------------------------------------
 
 {- 2.  elemAll and stopsAt  -  20% -}
 
 {- (a)  elemAll - 10%-}
 -- please don't include the myCatsLog list in your solution file. 
+elemAll :: [a] -> [a] -> Bool
+elemAll [] [] = True
+elemAll (x:xs) [] = False
+elemAll [] (y:ys) = True
+elemAll (x:xs) (y:ys) = False
 
 {- (b) stopsAt - 10%-}
 
@@ -18,8 +31,8 @@ module HW2
 {- 3. isBigger and applyRange - 25% -}
 
 --define the Timestamp datatype
-data Timestamp =  DATE (Int,Int,Int) |  DATETIME (Int,Int,Int,Int,Int) 
-                  deriving (Show, Eq)
+-- data Timestamp =  DATE (Int,Int,Int) |  DATETIME (Int,Int,Int,Int,Int) 
+--                   deriving (Show, Eq)
 
 {- (a)  isBigger - 15% -}
 
@@ -30,11 +43,11 @@ data Timestamp =  DATE (Int,Int,Int) |  DATETIME (Int,Int,Int,Int,Int)
 {-4 - foldTree, createRTree, fastSearch  - 35%-}
 
 --define Tree and RTree data types
-data Tree a = LEAF a | NODE a (Tree a) (Tree a)
-               deriving (Show,  Eq, Ord)
+-- data Tree a = LEAF a | NODE a (Tree a) (Tree a)
+--                deriving (Show,  Eq, Ord)
 
-data RTree a = RLEAF a | RNODE a (a,a) (RTree a) (RTree a)
-                    deriving (Show, Eq, Ord)
+-- data RTree a = RLEAF a | RNODE a (a,a) (RTree a) (RTree a)
+--                     deriving (Show, Eq, Ord)
 
 {- (a) foldTree - 8% -}
 
